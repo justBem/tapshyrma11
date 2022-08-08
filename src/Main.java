c813dfd9cec35545030504f8d7cfaf50bbeda837
Admin <bermetsaadat@gmail.com>
2022-08-08 20:23:14 +0600

    red

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      Triangle triangle=new Triangle();
+      Scanner scan=new Scanner(System.in);
+      triangle.a=scan.nextInt();
+      triangle.b=scan.nextInt();
+      triangle.c=scan.nextInt();
+        triangle.area(triangle.a,triangle.b,triangle.c);
     }
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Triangle {
+public class Triangle {
+    int a,b,c;
+    public static void area(int a,int b,int c){
+        double p=(a+b+c);
+        double ar=Math.sqrt((p*(p-a)*(p-b)*(p-c)));
+        System.out.println(ar);
+
+    }
 }
